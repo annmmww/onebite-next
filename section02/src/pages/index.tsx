d3f96a85314c982c248b7bc20cebc0d49bbbfb64
aNN-algorithm <kyukyu0314@naver.com>
2025-09-29 14:09:41 +0900

    2.6) 스타일링

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,4 +1,11 @@
 // index 페이지
+import style from "./index.module.css";
+
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+      <h1 className={style.h2}>H2</h1>
+    </>
+  );
 }
