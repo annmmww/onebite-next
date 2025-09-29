258ca81ce87141d1baadca93e5ef5a6bfdd28139
aNN-algorithm <kyukyu0314@naver.com>
2025-09-30 01:40:50 +0900

    2.12) SSR 2.실습 - search/index.tsx

--- section02/src/lib/fetch-books.ts
+++ section02/src/lib/fetch-books.ts
@@ -3,8 +3,12 @@ import { BookData } from "@/types";
 // 비동기로 반환하기 때문에 Promise
 // 반환하는 타입은 types.ts에 저장된 타입
 // 배열로 반환
-export default async function fetchBooks(): Promise<BookData[]> {
-  const url = `http://localhost:12345/book`; // 모든 도서의 데이터
+export default async function fetchBooks(q?: string): Promise<BookData[]> {
+  let url = `http://localhost:12345/book`; // 모든 도서의 데이터
+
+  if (q) {
+    url += `/search?q=${q}`;
+  }
 
   try {
     const response = await fetch(url);
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,11 +1,27 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-items";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
 // next/navigation은 App Router에 쓸 것
 
-export default function Page() {
+// 이 함수를 썼기 때문에, SSR 방식으로 작동하게 됨
+// context : 현재 브라우저로부터 받은 요청에 대한 모든 정보가 다 포함
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+
+  return {
+    props: { books },
+  };
+};
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       {books.map((book) => (
