7e0ee4077b56aea0edff718381ed487c0ae4ca5d
aNN-algorithm <kyukyu0314@naver.com>
2025-09-30 19:20:08 +0900

    3.6) 네비게이팅 - (1) 자바스크립트 번들 불러오기

--- section03/src/app/book/[id]/page.tsx
+++ section03/src/app/book/[id]/page.tsx
@@ -1,3 +1,5 @@
+import ClientComponent from "@/components/client-component";
+
 export default async function Page({
   params,
 }: {
@@ -5,5 +7,12 @@ export default async function Page({
 }) {
   const { id } = await params;
 
-  return <div>book/{id} page입니다.</div>;
+  return (
+    <div>
+      book/{id} page입니다.
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  );
 }
