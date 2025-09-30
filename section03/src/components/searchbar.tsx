0cf3bfa3ede0533d40148848868e577ae7ca222e
aNN-algorithm <kyukyu0314@naver.com>
2025-09-30 19:24:47 +0900

    3.6) 네비게이팅 - (2) 네비게이팅

--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,17 +1,26 @@
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+// 이제 앱 라우터 버전의 패키지인 next/navigation 의 useRouter를 import
+// 만약 next/router의 useRouter 가져오면 오류남!
 
 export default function Searchbar() {
+  const router = useRouter();
   const [search, setSearch] = useState("");
 
   // 이 change 이벤트는, HTMLInputElement에서 발생한 것
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
+
+  const onSubmit = () => {
+    router.push(`/search?q=${search}`);
+  };
+
   return (
     <div>
       <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 }
