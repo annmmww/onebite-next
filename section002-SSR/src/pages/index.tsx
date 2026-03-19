9f5526a4e13f2e0cec5daf8e6d4db9b4ce1be184
Ahn Yeonjeong <149382038+annmmww@users.noreply.github.com>
2026-03-19 13:54:20 +0900

    섹션 3. Page Router 핵심 정리 - SSR: fetch-books

---
+++ section002-SSR/src/pages/index.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from "react";
+import style from "./index.module.css";
+import SearchableLayout from "@/components/searchable-layout";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+import fetchBooks from "@/lib/fetch-books";
+import { InferGetServerSidePropsType } from "next";
+
+export const getServerSideProps = async () => {
+  const allBooks = await fetchBooks();
+
+  return {
+    props: {
+      allBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(allBooks);
+
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {allBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {allBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+Home.getLayout = function getLayout(page: ReactNode) {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
