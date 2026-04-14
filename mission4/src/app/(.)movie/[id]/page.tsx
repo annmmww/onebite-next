e2a8f13b0f1c175054a0409ede364dcc8184a129
Ahn Yeonjeong <149382038+annmmww@users.noreply.github.com>
2026-04-14 22:10:40 +0900

    init: mission4

--- mission4/src/app/(.)movie/[id]/page.tsx
+++ mission4/src/app/(.)movie/[id]/page.tsx
@@ -3,7 +3,7 @@ import MoviePage from "@/src/app/movie/[id]/page";
 export default function Page(props: any) {
   return (
     <div>
-      가로채기 성공!
+      가로채기 성공!!
       <MoviePage {...props} />
     </div>
   );
