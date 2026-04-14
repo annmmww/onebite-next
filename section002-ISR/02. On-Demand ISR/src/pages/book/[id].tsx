a7c7ab4ab76073c5f6d1ac937f56556ca561153e
Ahn Yeonjeong <149382038+annmmww@users.noreply.github.com>
2026-04-14 21:55:29 +0900

    섹션 3. Page Router 핵심 정리 - On-Demand ISR

---
+++ section002-ISR/02. On-Demand ISR/src/pages/book/[id].tsx
@@ -0,0 +1,64 @@
+import fetchOneBook from "@/lib/fetch-one-book";
+import style from "@/pages/book/[id].module.css";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const id = context.params!.id;
+  const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { book },
+  };
+};
+
+export default function Page({
+  book,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) return <div>로딩 중입니다.</div>;
+
+  if (!book) {
+    return <div>도서를 찾을 수 없습니다.</div>;
+  }
+
+  const { title, subTitle, description, author, publisher, coverImgUrl } = book;
+
+  return (
+    <div className={style.container}>
+      <div
+        className={style.cover_img_container}
+        style={{
+          backgroundImage: `url('${coverImgUrl}')`,
+        }}
+      >
+        <Image src={coverImgUrl} alt={title} width={200} height={300} />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
+    </div>
+  );
+}
