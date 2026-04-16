efae9ca0a1211e0abf3f2677c170d8bbf403bd4b
Ahn Yeonjeong <149382038+annmmww@users.noreply.github.com>
2026-04-16 13:22:50 +0900

    3. Page Router 핵심 정리

---
+++ section03/05. SEO 설정하기/src/pages/index.tsx
@@ -0,0 +1,59 @@
+import { ReactNode } from "react";
+import style from "./index.module.css";
+import SearchableLayout from "@/components/searchable-layout";
+import BookItem from "@/components/book-item";
+import fetchBooks from "@/lib/fetch-books";
+import { InferGetServerSidePropsType } from "next";
+import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
+
+export const getStaticProps = async () => {
+  const [allBooks, randomBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      randomBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+  randomBooks,
+}: InferGetServerSidePropsType<typeof getStaticProps>) {
+  return (
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {randomBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
+  );
+}
+
+Home.getLayout = function getLayout(page: ReactNode) {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
